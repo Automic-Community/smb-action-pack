102481ece20cd76c20bd3acd46445f3afe842cfc
TANT-A01\traso02 <son.tran@ca.com>
2018-11-23 10:02:51 +0700

    correct typo

--- tools/smb-tool/src/main/java/com/uc4/ara/feature/dirfilehandling/GetFiles.java
+++ tools/smb-tool/src/main/java/com/uc4/ara/feature/dirfilehandling/GetFiles.java
@@ -130,11 +130,11 @@ public class GetFiles extends AbstractPublicFeature {
 
         String recursiveStr = parser.getOptionValue(recursive);
         String overwriteStr = parser.getOptionValue(overwrite);
-        String perserveStr = parser.getOptionValue(preserve);
+        String preserveStr = parser.getOptionValue(preserve);
 
         boolean recursiveValue = recursiveStr !=null && recursiveStr.equalsIgnoreCase("no") ? false : true;
         boolean overwriteValue = overwriteStr != null && overwriteStr.equalsIgnoreCase("yes") ? true : false;
-        boolean preserveValue = perserveStr != null && perserveStr.equalsIgnoreCase("yes") ? true : false;
+        boolean preserveValue = preserveStr != null && preserveStr.equalsIgnoreCase("yes") ? true : false;
         if(usernameValue == null) usernameValue = "";
         if(passwordValue == null) passwordValue = "";
 
