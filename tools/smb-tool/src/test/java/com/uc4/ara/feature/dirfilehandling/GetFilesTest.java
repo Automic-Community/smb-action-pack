476c4dc8cae7fef3933af26b8fe2c2850908c28a
TANT-A01\traso02 <son.tran@ca.com>
2019-02-14 13:56:43 +0700

    Update test cases

--- tools/smb-tool/src/test/java/com/uc4/ara/feature/dirfilehandling/GetFilesTest.java
+++ tools/smb-tool/src/test/java/com/uc4/ara/feature/dirfilehandling/GetFilesTest.java
@@ -2,9 +2,20 @@ package com.uc4.ara.feature.dirfilehandling;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+
+import org.junit.Before;
 import org.junit.Test;
 
+import com.uc4.ara.feature.utils.FileUtil;
+
 public class GetFilesTest {
+	private static final String targetDirectory = "C:/test/One_Installer/";
+
+	@Before
+	public void setUp() {
+		FileUtil.deleteDirectory(new File(targetDirectory));
+	}
 
 	@Test
 	public void testGetLargeFile_ShouldSuccess() throws Exception {
@@ -16,8 +27,8 @@ public class GetFilesTest {
 				"-p", "139",
 				"-u", "Administrator",
 				"-pwd", "S3rv3r@Aut",
-				"-src", "automic/smb/JDK/jdk-11u13",
-				"-tgt", "C:\\test\\One_Installer",
+				"-src", "automic/smb/Webspherev9-trial/WPE_TRIAL_PART_4_V9.0.0_MP_ML.zip",
+				"-tgt", targetDirectory + "Webspherev9-trial/WPE_TRIAL_PART_4_V9.0.0_MP_ML.zip",
 				"-o", "YES",
 				"-to", "5000",
 				"-prv", "YES",
@@ -41,7 +52,31 @@ public class GetFilesTest {
 				"-u", "Administrator",
 				"-pwd", "S3rv3r@Aut",
 				"-src", "automic/smb/APP_SRV/apache-tomcat-9.0.14",
-				"-tgt", "C:/test/One_Installer/APP_SRV/apache-tomcat-9.0.14",
+				"-tgt", targetDirectory + "APP_SRV/apache-tomcat-9.0.14",
+				"-o", "YES",
+				"-to", "5000",
+				"-prv", "YES",
+				"-r", "YES",
+				"-smbDN", ""};
+		long start = System.currentTimeMillis();
+		int retCode = test.run(args);
+		long end = System.currentTimeMillis();
+		System.out.println("DEBUG: Get large file took " + (end - start)/1000 + " seconds");
+		assertEquals(0, retCode);
+	}
+	
+	@Test
+	public void testGetSmallFile_ShouldSuccess() throws Exception {
+		GetFiles test = new GetFiles();
+		test.initialize();
+		String[] args = new String[] {
+				"-prc", "SMB",
+				"-h", "VVNSATSTNDRD01",
+				"-p", "139",
+				"-u", "Administrator",
+				"-pwd", "S3rv3r@Aut",
+				"-src", "automic/smb/DB/jdbc/sqljdbc42.jar",
+				"-tgt", targetDirectory + "DB/jdbc/sqljdbc42.jar",
 				"-o", "YES",
 				"-to", "5000",
 				"-prv", "YES",
@@ -54,7 +89,6 @@ public class GetFilesTest {
 		assertEquals(0, retCode);
 	}
 
-	@Test
 	public void testGetFileFromPD01_ShouldSuccess() throws Exception {
 		GetFiles test = new GetFiles();
 		test.initialize();
@@ -65,7 +99,7 @@ public class GetFilesTest {
 				"-u", "admin",
 				"-pwd", "password",
 				"-src", "stg\\qa\\EM\\1.0.0\\+\\*.zip",
-				"-tgt", "C:\\test\\One_Installer",
+				"-tgt", targetDirectory,
 				"-o", "YES",
 				"-to", "5000",
 				"-prv", "YES",
@@ -78,7 +112,6 @@ public class GetFilesTest {
 		assertEquals(0, retCode);
 	}
 	
-	@Test
 	public void testGetFileFromFsu_ShouldSuccess() throws Exception {
 		GetFiles test = new GetFiles();
 		test.initialize();
